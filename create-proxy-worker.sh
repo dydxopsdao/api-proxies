ebd1cbb60baa5d6d5c89b2b09ddd60c39a85f1ba
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-07-25 15:34:49 +0200

    Improve script wording

--- create-proxy-worker.sh
+++ create-proxy-worker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# CloudFlare worker interactive creation script.
+# CloudFlare proxy worker interactive creation script.
 # See: https://developers.cloudflare.com/workers/get-started/guide/
 
 # Abort on error
 set -e
 
-# Static path to the Worker template
+# Static path to the proxy worker template
 GITHUB_TEMPLATE_PATH=dydxopsdao/api-proxies/worker-template
 
 # Worker specification
@@ -20,7 +20,7 @@ function prompt {
   read -p "$1: " $2
 }
 
-prompt "Name of the worker (e.g.: my-worker)" WORKER_NAME
+prompt "Name of the worker (e.g.: my-proxy)" WORKER_NAME
 prompt "API endpoint (e.g. https://api.example.com)" API_ENDPOINT
 prompt "CORS origin (e.g. https://dydx.trade)" CORS_ORIGIN
 prompt "Credential header name (e.g. Authorization)" CREDENTIAL_HEADER
@@ -29,7 +29,7 @@ prompt "Credential header value (e.g. Bearer: secretvalue)" CREDENTIAL_VALUE
 # Login to your Cloudflare account. Follow the link that will be printed.
 npx wrangler login --browser false
 
-# Create a new Worker project using the template
+# Create a new worker project using the template
 # See: https://www.npmjs.com/package/create-cloudflare
 npm create cloudflare -- $WORKER_NAME \
   --type remote-template \
@@ -40,7 +40,7 @@ npm create cloudflare -- $WORKER_NAME \
 # Change into the newly created project directory
 cd $WORKER_NAME
 
-# Create the Worker. You may be prompted to select a Cloudflare account.
+# Create the worker. You may be prompted to select a Cloudflare account.
 # See: https://developers.cloudflare.com/workers/wrangler/commands/#deploy
 npx wrangler deploy \
   --var API_ENDPOINT:$API_ENDPOINT \
