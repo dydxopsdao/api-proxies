8c5ba196af91705349f2e550d5cfecdf207846d9
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-07-25 14:40:41 +0200

    Abstract credential header setup

--- create-proxy-worker.sh
+++ create-proxy-worker.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
 
-GIT_TEMPLATE_PATH=dydxopsdao/api-proxies/worker-template
+# CloudFlare worker interactive creation script.
+# See: https://developers.cloudflare.com/workers/get-started/guide/
+
+# Abort on error
+set -e
+
+# Static path to the Worker template
+GITHUB_TEMPLATE_PATH=dydxopsdao/api-proxies/worker-template
+
+# Worker specification
+WORKER_NAME=interactive
+API_ENDPOINT=interactive
+CORS_ORIGIN=interactive
+CREDENTIAL_HEADER=interactive
+CREDENTIAL_VALUE=interactive
 
 function prompt {
   read -p "$1: " $2
 }
 
-prompt "Enter the name of the worker (e.g.: my-worker)" WORKER_NAME
-prompt "Enter the API endpoint (e.g.: https://api.example.com)" API_ENDPOINT
-prompt "Enter the API key" API_KEY
-prompt "Enter the CORS allow origin (e.g.: https://dydx.trade)" CORS_ALLOW_ORIGIN
+prompt "Name of the worker (e.g.: my-worker)" WORKER_NAME
+prompt "API endpoint (e.g. https://api.example.com)" API_ENDPOINT
+prompt "CORS origin (e.g. https://dydx.trade)" CORS_ORIGIN
+prompt "Credential header name (e.g. Authorization)" CREDENTIAL_HEADER
+prompt "Credential header value (e.g. Bearer: secretvalue)" CREDENTIAL_VALUE
 
-# Login to your Cloudflare account. Follow the link that will be printed to the console.
+# Login to your Cloudflare account. Follow the link that will be printed.
 npx wrangler login --browser false
 
 # Create a new Worker project using the template
-npm create cloudflare -- $WORKER_NAME --type remote-template --template $GIT_TEMPLATE_PATH --deploy false --ts false
+# See: https://www.npmjs.com/package/create-cloudflare
+npm create cloudflare -- $WORKER_NAME \
+  --type remote-template \
+  --template $GITHUB_TEMPLATE_PATH \
+  --deploy false \
+  --ts false
 
 # Change into the newly created project directory
 cd $WORKER_NAME
 
 # Create the Worker. You may be prompted to select a Cloudflare account.
+# See: https://developers.cloudflare.com/workers/wrangler/commands/#deploy
 npx wrangler deploy \
   --var API_ENDPOINT:$API_ENDPOINT \
-  --var CORS_ALLOW_ORIGIN:$CORS_ALLOW_ORIGIN
+  --var CORS_ORIGIN:$CORS_ORIGIN \
+  --var CREDENTIAL_HEADER:$CREDENTIAL_HEADER
 
-# Redeploy the worker with the API_KEY variable
-echo $API_KEY | npx wrangler secret put API_KEY
+# Redeploy the worker with a secret
+echo $CREDENTIAL_VALUE | npx wrangler secret put CREDENTIAL_VALUE
+
+echo "Worker created successfully!"
--- worker-template/js/src/index.js
+++ worker-template/js/src/index.js
@@ -1,38 +1,43 @@
 /**
- A simple proxy that adds a bearer token to the outgoing request.
+ A simple proxy that:
+ - decorates the outgoing request with a custom authentication token,
+ - decorates the response from the destination host with CORS headers.
 
  parseUri code from: https://blog.stevenlevithan.com/archives/parseuri
  */
 
- import { parseUri } from './parseUri.js';
+import { parseUri } from './parseUri.js';
 
- export default {
-   async fetch(request, env, ctx) {
-	 const requestUri = parseUri(request.url)
-	 const callUrl = `${env.API_ENDPOINT}${requestUri.relative}`
- 
-	 const apiResponse = await fetch(
-	   callUrl,
-	   {
-		 method: request.method,
-		 headers: {
-		   "Authorization": `Bearer: ${env.API_KEY}`,
-		 }
-	   }
-	 );
- 
-	 const newResponse = new Response(apiResponse.body, {
-	   status: apiResponse.status,
-	   statusText: apiResponse.statusText,
-	   headers: {
-		 ...apiResponse.headers,
-		 "Access-Control-Allow-Origin": "*",  // TODO: change to https://dydx.trade or appropriate domain
-		 "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-		 "Access-Control-Allow-Headers": "Content-Type, Authorization, X-ACCESS-KEY",
-	   }
-	 });
- 
-	 return newResponse;
-   },
- };
- 
+export default {
+	async fetch(request, env, ctx) {
+		const requestUri = parseUri(request.url)
+		const callUrl = `${env.API_ENDPOINT}${requestUri.relative}`
+
+		try {
+			const apiResponse = await fetch(
+				callUrl,
+				{
+					method: request.method,
+					headers: {
+						[env.CREDENTIAL_HEADER]: env.CREDENTIAL_VALUE,
+					}
+				}
+			);
+
+			const newResponse = new Response(apiResponse.body, {
+				status: apiResponse.status,
+				statusText: apiResponse.statusText,
+				headers: {
+					...apiResponse.headers,
+					"Access-Control-Allow-Origin": env.CORS_ORIGIN,
+					"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+					"Access-Control-Allow-Headers": `Content-Type, ${env.CREDENTIAL_HEADER}`,
+				}
+			});
+
+			return newResponse;
+		} catch (error) {
+			return new Response("Error fetching from API", { status: 500 });
+		}
+	},
+};
