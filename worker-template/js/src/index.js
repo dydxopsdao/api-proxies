574d30f1778fc795304059a8147e2c9e4cde1ac7
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-07-26 12:52:56 +0200

    Reformat JS

--- worker-template/js/src/index.js
+++ worker-template/js/src/index.js
@@ -2,46 +2,44 @@
  A simple proxy that:
  - decorates the outgoing request with a custom authentication token,
  - decorates the response from the destination host with CORS headers.
-
- parseUri code from: https://blog.stevenlevithan.com/archives/parseuri
  */
 
-import { parseUri } from './parseUri.js';
+import { parseUri } from './parseUri.js';  // https://blog.stevenlevithan.com/archives/parseuri
 
 const METHODS_WITH_BODY = ["POST", "PUT", "PATCH"];
 
 export default {
-	async fetch(request, env, ctx) {
-		const requestUri = parseUri(request.url)
-		const callUrl = `${env.API_ENDPOINT}${requestUri.relative}`
+  async fetch(request, env, ctx) {
+    const requestUri = parseUri(request.url)
+    const callUrl = `${env.API_ENDPOINT}${requestUri.relative}`
 
-		try {
-			const apiResponse = await fetch(
-				callUrl,
-				{
-					method: request.method,
-					headers: {
-						"Accept": "application/json",
-						[env.CREDENTIAL_HEADER]: env.CREDENTIAL_VALUE,
-					},
-					body: METHODS_WITH_BODY.includes(request.method) ? await request.text() : undefined,
-				}
-			);
+    try {
+      const apiResponse = await fetch(
+        callUrl,
+        {
+          method: request.method,
+          headers: {
+            "Accept": "application/json",
+            [env.CREDENTIAL_HEADER]: env.CREDENTIAL_VALUE,
+          },
+          body: METHODS_WITH_BODY.includes(request.method) ? await request.text() : undefined,
+        }
+      );
 
-			const newResponse = new Response(apiResponse.body, {
-				status: apiResponse.status,
-				statusText: apiResponse.statusText,
-				headers: {
-					...apiResponse.headers,
-					"Access-Control-Allow-Origin": env.CORS_ORIGIN,
-					"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-					"Access-Control-Allow-Headers": `Content-Type, ${env.CREDENTIAL_HEADER}`,
-				}
-			});
+      const newResponse = new Response(apiResponse.body, {
+        status: apiResponse.status,
+        statusText: apiResponse.statusText,
+        headers: {
+          ...apiResponse.headers,
+          "Access-Control-Allow-Origin": env.CORS_ORIGIN,
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": `Content-Type, ${env.CREDENTIAL_HEADER}`,
+        }
+      });
 
-			return newResponse;
-		} catch (error) {
-			return new Response("Error fetching from API", { status: 500 });
-		}
-	},
+      return newResponse;
+    } catch (error) {
+      return new Response("Error fetching from API", { status: 500 });
+    }
+  },
 };
